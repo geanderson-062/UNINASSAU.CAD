25a0e0237621460dec5b79f5447df3837f50a9d1
geanderson062 <geanderson062@outlook.com>
2022-04-03 15:16:52 -0300

    03/04/2022
    telas de login coordenador e adm.

--- resources/JS/exibir_senha.js
+++ resources/JS/exibir_senha.js
@@ -2,7 +2,7 @@
 
  function mostrarOcultarSenha(){
 
-    var senha=document.getElementById("exampleInputPassword1");
+    var senha=document.getElementById("floatingPassword");
 
     if(senha.type=="password"){
             senha.type="text";
