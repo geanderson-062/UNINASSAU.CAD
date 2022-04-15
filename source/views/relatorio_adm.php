ae4cf5c1786fa713c59579372dde0263fc7526f2
geanderson062 <geanderson062@outlook.com>
2022-04-15 15:36:26 -0300

    corrigindo caminhos

---
+++ source/views/relatorio_adm.php
@@ -0,0 +1,17 @@
+<?php
+include '../controller/FPDF/fpdf.php';
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Relatorio do Administrador!');
+$pdf->Output();
+
+
+
+
+
+
+
+?>
+
--- source/views/relatorio_coordenador.php
+++ source/views/relatorio_coordenador.php
@@ -4,7 +4,7 @@ include '../controller/FPDF/fpdf.php';
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,'Relatorio do Coordenador!');
 $pdf->Output();
 
 
